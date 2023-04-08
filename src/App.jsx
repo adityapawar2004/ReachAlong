c1ad29e814e9d0026f1446a46f4212175c544618
Aditya Pawar <pawaraditya2901@gmail.com>
2023-04-08 19:01:00 +0530

    i had fixed the error

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Home from './components/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div style={{ height: "100vh", backgroundImage: 'url("https://unbounce.com/photos/Gradient-Background.png")', backgroundRepeat: "no-repeat", width: "100vw", backgroundSize: "cover", }}>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </Router>
+      {/* <Home /> */}
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   )
 }
